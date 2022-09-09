6cb4419855a8c7874c71dc4bf20e005f55577363
HYNCM <cmhyn1015@gmail.com>
2022-09-09 14:29:20 +0800

    fix a bug

--- array.ts
+++ array.ts
@@ -54,5 +54,3 @@ function logArrayElements(element, index, array) {
   console.log("a[" + index + "] = " + element);
 }
 [2, 5, , 9].forEach(logArrayElements);
-// logs:
-// a[0] = 2
--- js/array.js
+++ js/array.js
@@ -46,5 +46,3 @@ function logArrayElements(element, index, array) {
     console.log("a[" + index + "] = " + element);
 }
 [2, 5, , 9].forEach(logArrayElements);
-// logs:
-// a[0] = 2
